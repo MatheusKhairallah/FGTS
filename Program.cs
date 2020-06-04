b142bb72cb0d6215259a80ada7ebd0dce24e5649
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:40:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número que representara um salário de um funcionário,e logo após será feito o calculo da parcela do FGTS de 8% em cima deste salário.");
+            Console.Write("Sálario:");
+            double salario=Convert.ToDouble(Console.ReadLine());
+            double resultado=salario * 8;
+            double resultado2=resultado / 100;
+            Console.Clear();
+            Console.WriteLine($"A parcela do FGTS é:{resultado2:C}");
+        }
+    }
+}
